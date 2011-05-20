7f35f87161f698b0dfa0526e8104b3c05c30405f
Pete de Zwart <dezwart@gmail.com>
2011-05-20 19:48:17 +1000

    Basic dynamic DNS parameters and templates added. The zone files contents are unmanaged.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,15 @@
 #   Specify one or more recursive forwarders as IP addresses in dotted quad
 #   format.
 #
+# [*dynamic_dns_key*]
+#   hmac-md5 key used for dynamic DNS updates. Default is unset.
+#
+# [*dynamic_dns_forward_zone*]
+#   Forward DNS zone for dynamic updates. The zone file contents are unmanaged.
+#
+# [*dynamic_dns_reverse_zone*]
+#   Reverse DNS zone for dynamic updates. The zone file contents are unmanaged.
+#
 # == Variables
 #
 # == Examples
@@ -14,7 +23,10 @@
 #        forwarders => [ '10.0.0.1', '10.0.0.2' ],
 #    }
 #
-class bind( $forwarders = undef ) {
+class bind( $forwarders = undef,
+		$dynamic_dns_key = undef,
+		$dynamic_dns_forward_zone = undef,
+		$dynamic_dns_reverse_zone = undef ) {
 	package { 'bind9':
 		ensure	=> installed,
 	}
@@ -35,5 +47,52 @@ class bind( $forwarders = undef ) {
 		restart		=> '/etc/init.d/bind9 reload',
 		require		=> Package['bind9'],
 		subscribe	=> File['/etc/bind/named.conf.options'],
+	}
+
+	if $dynamic_dns_key {
+		file { '/etc/bind':
+			ensure	=> directory,
+			owner	=> root,
+			group	=> bind,
+			mode	=> '2774',
+		}
+			
+		file { '/etc/bind/dynamic-dns.key':
+			ensure	=> file,
+			owner	=> root,
+			group	=> bind,
+			mode	=> '0640',
+			content	=> template('bind/dynamic-dns.key.erb'),
+			require	=> Package['bind9'],
+			notify	=> Service['bind9'],
+		}
+
+		file { '/etc/bind/named.conf.local':
+			ensure	=> file,
+			owner	=> root,
+			group	=> bind,
+			mode	=> '0640',
+			content	=> template('bind/named.conf.local.erb'),
+			require	=> Package['bind9'],
+			notify	=> Service['bind9'],
+		}
+
+		file { "/etc/bind/db.$dynamic_dns_forward_zone":
+			ensure	=> file,
+			owner	=> root,
+			group	=> bind,
+			mode	=> '0660',
+			require	=> Package['bind9'],
+			notify	=> Service['bind9'],
+		}
+
+		file { "/etc/bind/db.$dynamic_dns_reverse_zone":
+			ensure	=> file,
+			owner	=> root,
+			group	=> bind,
+			mode	=> '0660',
+			require	=> Package['bind9'],
+			notify	=> Service['bind9'],
+		}
 	}
 }
