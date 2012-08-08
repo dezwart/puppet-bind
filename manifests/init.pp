0aa55c1e39cbe25b604cd48b34253d0498be2879
Mike Fuller <mfuller@atlassian.com>
2012-08-08 11:33:13 +1000

    added key definition to named.local.conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -98,4 +98,16 @@ class bind( $forwarders = undef,
         content => template("bind/named.conf.local_preamble.erb"),
         notify  => Exec['named_conf_local_file_assemble'],
     }
+
+    if $key_name and $key_algorithm and $key_secret {
+        file { "$bind::named_conf_local_file_fragments_directory/03_named.conf.local_key_fragment_$name":
+            ensure => file,
+            owner => root,
+            group => $bind::group,
+            mode => '0644',
+            content => template('bind/named.conf.local_key_fragment.erb'),
+            require => File[$bind::named_conf_local_file_fragments_directory],
+            notify => Exec[$bind::named_conf_local_file_assemble],
+        }
+    }
 }
