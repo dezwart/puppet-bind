72117579e4eaeb64250d89461764433d13489568
Pete de Zwart <pdzwart@atlassian.com>
2013-02-13 10:12:18 +1100

    Added in remaining basic shoulds for bind class.

--- spec/classes/bind_spec.rb
+++ spec/classes/bind_spec.rb
@@ -8,6 +8,11 @@ describe 'bind' do
   group = cn
   conf_dir = "/etc/#{cn}"
   zone_dir = "/var/lib/#{cn}"
+  nco = "#{conf_dir}/named.conf.options"
+  ncl = "#{conf_dir}/named.conf.local"
+  ncl_ffd = "#{ncl}.d"
+  ncl_file_assemble = 'ncl_file_assemble'
+  ncl_preamble = "#{ncl_ffd}/00_named.conf.local_preamble"
 
   context 'default' do
     it {
@@ -26,6 +31,28 @@ describe 'bind' do
       should contain_file(zone_dir).with({
         'ensure' => 'directory',
       })
+    }
+
+    it {
+      should contain_file(nco).with({
+        'ensure' => 'file',
+      })
+    }
+
+    it {
+      should contain_file(ncl).with({
+        'ensure' => 'file',
+      })
+
+      should contain_file(ncl_ffd).with({
+        'ensure' => 'directory',
+      })
+
+      should contain_file(ncl_preamble).with({
+        'ensure' => 'file',
+      })
+
+      should contain_exec(ncl_file_assemble)
     }
   end
 end
